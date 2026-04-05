e7daaa6fa7c867c1470666360baa30ec17997693
Adish Jain <adish.jain@myntra.com>
2026-04-05 13:11:51 +0530

    added projects page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import Chatbot from "./components/Chatbot";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProjectsPage from "./pages/ProjectsPage";
+import MLChatbotPage from "./pages/MLChatbotPage";
 
 function App() {
-  return <Chatbot />;
+  return (
+    <Routes>
+      <Route path="/" element={<ProjectsPage />} />
+      <Route path="/projects" element={<ProjectsPage />} />
+      <Route path="/ml-chatbot" element={<MLChatbotPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
