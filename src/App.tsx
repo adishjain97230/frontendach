26851e78bffc1dff8b618d7af35b563ed59c5d3d
Adish Jain <adish.jain@myntra.com>
2026-04-06 18:22:59 +0530

    dummy wordle page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import ProjectsPage from "./pages/ProjectsPage";
 import MLChatbotPage from "./pages/MLChatbotPage";
+import WordleGame from "./pages/WordleGame";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Route path="/" element={<ProjectsPage />} />
       <Route path="/projects" element={<ProjectsPage />} />
       <Route path="/ml-chatbot" element={<MLChatbotPage />} />
+      <Route path="/wordle" element={<WordleGame />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
