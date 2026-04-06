7151eeee430cdf1c67a57bf711b605b6acb69f34
Adish Jain <adish.jain@myntra.com>
2026-04-06 19:03:53 +0530

    added wordle basic functionality

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -4,3 +4,11 @@ export const API_BASE_URL = "https://backendach.duckdns.org";
 export const CHATBOT_CHAT_PATH = "/machine-learning/chatbot/chat/";
 
 export const CHATBOT_CHAT_URL = `${API_BASE_URL}${CHATBOT_CHAT_PATH}`;
+
+export const WORDLE_GET_WORD_PATH = "/wordle/get-word/";
+
+export const WORDLE_CHECK_WORD_PATH = "/wordle/check-word/";
+
+export const WORDLE_GET_WORD_URL = `${API_BASE_URL}${WORDLE_GET_WORD_PATH}`;
+
+export const WORDLE_CHECK_WORD_URL = `${API_BASE_URL}${WORDLE_CHECK_WORD_PATH}`;
--- src/pages/WordlePage.tsx
+++ src/pages/WordlePage.tsx
@@ -1,3 +1,75 @@
+import { useState } from "react";
+import { WORDLE_CHECK_WORD_URL, WORDLE_GET_WORD_URL } from "../constants/api";
+
 export default function WordlePage() {
-  return <div>Wordle</div>;
+  const [currentWord, setCurrentWord] = useState("");
+  const [words, setWords] = useState<string[]>([]);
+  const [feedbacks, setFeedbacks] = useState<number[][]>([]);
+  let targetWordId = 0;
+
+  const handleSubmit = async () => {
+    if (currentWord.length !== 5) {
+      return;
+    } else {
+      setWords((prev) => [...prev, currentWord]);
+      setCurrentWord("");
+      if (words.length === 0) {
+        const word_response = await fetch(WORDLE_GET_WORD_URL, {
+          method: "GET",
+          headers: {},
+        });
+        if (!word_response.ok) {
+          return;
+        }
+        const word_data = await word_response.json();
+        targetWordId = word_data.word_id;
+      }
+
+      const feedback_response = await fetch(WORDLE_CHECK_WORD_URL, {
+        method: "POST",
+        headers: {},
+        body: JSON.stringify({
+          word_id: targetWordId,
+          guess: currentWord,
+          guess_number: words.length,
+        }),
+      });
+      if (!feedback_response.ok) {
+        return;
+      }
+      const feedback_data = await feedback_response.json();
+      setFeedbacks((prev) => [...prev, feedback_data.feedback]);
+      return;
+    }
+  };
+
+  return (
+    <>
+      <ul>
+        {words.map((word) => (
+          <li key={word}>{word}</li>
+        ))}
+      </ul>
+      <ul>
+        {feedbacks.map((feedback) => (
+          <li key={feedback.join("")}>{feedback.join("")}</li>
+        ))}
+      </ul>
+      <textarea
+        id="wordle-input"
+        value={currentWord}
+        onChange={(e) =>
+          setCurrentWord(
+            e.target.value
+              .toLowerCase()
+              .replace(/[^a-z]/g, "")
+              .slice(0, 5),
+          )
+        }
+      ></textarea>
+      <button id="wordle-submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </>
+  );
 }
